dc4274d6918151a8067357e5785fe991e3273450
Johnny Miller (ZA) <johnnysviva@outlook.com>
2022-09-04 10:33:25 +0800

    perf($SQLite): use recommended setting for SQLite
    Recommended Settings
    https://docs.peewee-orm.com/en/latest/peewee/database.html#recommended-settings

--- python_boilerplate/repository/model/base_model.py
+++ python_boilerplate/repository/model/base_model.py
@@ -1,13 +1,27 @@
-from pathlib import Path
+from typing import Final
 
 from loguru import logger
 from peewee import Model, SqliteDatabase
 
 from python_boilerplate.common.common_function import get_data_dir, get_module_name
 
-_db_path: Path = get_data_dir() / f"{get_module_name()}.db"
-database: SqliteDatabase = SqliteDatabase(_db_path)
-logger.warning(f"SQLite database created. Path: [{_db_path}], {database}")
+# Database https://docs.peewee-orm.com/en/latest/peewee/database.html
+# Recommended Settings https://docs.peewee-orm.com/en/latest/peewee/database.html#recommended-settings
+# Logging queries https://docs.peewee-orm.com/en/latest/peewee/database.html#logging-queries
+
+DATABASE_PATH: Final = get_data_dir() / f"{get_module_name()}.db"
+DATABASE: Final = SqliteDatabase(
+    DATABASE_PATH,
+    pragmas={
+        # WAL-mode. https://sqlite.org/wal.html
+        "journal_mode": "wal",
+        # 64MB cache.
+        "cache_size": 64 * -1024,
+        # Let the OS manage syncing.
+        "synchronous": 0,
+    },
+)
+logger.warning(f"SQLite database created. Path: [{DATABASE_PATH}], {DATABASE}")
 
 
 class BaseModel(Model):
@@ -23,9 +37,11 @@ class BaseModel(Model):
         Meta configuration is passed on to subclasses, so our project’s models will all subclass BaseModel.
         There are many different attributes you can configure using Model.Meta.
 
-        Model options and table metadata https://docs.peewee-orm.com/en/latest/peewee/models.html#model-options-and-table-metadata
-        Primary Keys, Composite Keys and other Tricks https://docs.peewee-orm.com/en/latest/peewee/models.html#primary-keys-composite-keys-and-other-tricks
+        Model options and table metadata
+        https://docs.peewee-orm.com/en/latest/peewee/models.html#model-options-and-table-metadata
+        Primary Keys, Composite Keys and other Tricks
+        https://docs.peewee-orm.com/en/latest/peewee/models.html#primary-keys-composite-keys-and-other-tricks
         """
 
-        database = database
+        database = DATABASE
         legacy_table_names = False
