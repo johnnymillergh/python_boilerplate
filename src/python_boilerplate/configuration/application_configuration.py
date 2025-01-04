d8decbdfab1189139b84c91bfbdafc3f62546745
Johnny Miller <johnnysviva@outlook.com>
2025-01-04 17:21:16 +0800

    refactor(Poetry): pyproject.toml path fallback for PyInstaller scenario

--- src/python_boilerplate/configuration/application_configuration.py
+++ src/python_boilerplate/configuration/application_configuration.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import Final
 
 import tomllib
@@ -12,11 +13,19 @@ from python_boilerplate.common.common_function import (
 # `application_conf` contains the configuration for the application.
 application_conf: Final = ConfigFactory.parse_file(get_resources_dir() / "application.conf")
 
-_pyproject_toml_file: Final = PROJECT_ROOT_PATH / "pyproject.toml"
-if not _pyproject_toml_file.exists() or not _pyproject_toml_file.is_file():
-    error = f"File not found: {_pyproject_toml_file}"
-    logger.error(error)
-    raise FileNotFoundError(error)
+
+def check_existence(file_path: Path) -> bool:
+    return file_path.exists() and file_path.is_file()
+
+
+_pyproject_toml_file = PROJECT_ROOT_PATH / "pyproject.toml"
+if not check_existence(_pyproject_toml_file):
+    logger.warning(f"File not found: {_pyproject_toml_file}")
+    _pyproject_toml_file = Path(__file__).parent.parent.parent / "pyproject.toml"
+    logger.warning(f"Fallback to: {_pyproject_toml_file}")
+    if not check_existence(_pyproject_toml_file):
+        logger.error(f"Fallback failed due to file not found: {_pyproject_toml_file}")
+        raise FileNotFoundError(f"File not found: {_pyproject_toml_file}")
 with _pyproject_toml_file.open("rb") as file:
     pyproject_toml: Final = tomllib.load(file)
 
